9ae5ccf0eec0c46bca000b77a062a6bb6bad79c2
Cyneric <info@cyneric.de>
2013-12-04 23:45:06 +0100

    files added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>MediaGallery</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="header" width=100% height=auto >
+<div id="logo"><img src="images/logo.png" alt="MediaGallery" style="max-width:50%;"></div>
+</div>
+<div id="navigation">Bilder<div>
+</body>
+</html>
